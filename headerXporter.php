4f81f45566f8c356fff826c7e6dc9a12953b83ae
PhilippD <dippelp@hochschule-trier.de>
2017-08-12 16:50:55 +0200

    init Git commit

---
+++ headerXporter.php
@@ -0,0 +1,18 @@
+<?php
+  /*
+  Plugin Name: HeaderXPorter
+  Plugin URI: https://dippel.rocks
+  Description: a Plugin to export and Import X Pro (Themeco) Theme Headers.
+  Version: 1.0
+  Author: Philipp Dippel
+  Author URI: https://dippel.rocks
+  Licencse: MIT
+  */
+
+defined('ABSPATH') || exit();
+
+add_action('admin_menu', 'hxp_HeaderXPorter');
+
+
+
+ ?>
