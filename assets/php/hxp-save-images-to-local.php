814a560273bcac84bb4440635dc3db2d70a4660a
PhilippD <dippelp@hochschule-trier.de>
2017-08-28 05:20:14 +0200

    Begin to work on loading image php function

---
+++ assets/php/hxp-save-images-to-local.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *  * Created by PhpStorm.
+ * User: Philipp Dippel Inf | DMS - M
+ * For Project: headerXporter
+ * Date: 28.08.17
+ * Copyright: Philipp Dippel
+ */
+
+defined( 'ABSPATH' ) || exit();
+
+add_action('wp_ajax_hxp_save_image', 'hxp_save_image');
+
+function hxp_save_image($hxp_image_url) {
+
+
+
+
+}
